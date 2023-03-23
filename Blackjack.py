5e9332fa0193510343f0ce45815b96ec84109a5f
Nick Burton <125829913+nickburtondev@users.noreply.github.com>
2023-03-23 18:00:20 -0400

    Add files via upload

--- Blackjack.py
+++ Blackjack.py
@@ -1,32 +1,69 @@
 import random
+
 # Global variables
-suits = suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
-ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
-values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 'Nine':9, 'Ten':10, 'Jack':10, 'Queen':10, 'King':10, 'Ace':11}
+suits = suits = ("Hearts", "Diamonds", "Spades", "Clubs")
+ranks = (
+    "Two",
+    "Three",
+    "Four",
+    "Five",
+    "Six",
+    "Seven",
+    "Eight",
+    "Nine",
+    "Ten",
+    "Jack",
+    "Queen",
+    "King",
+    "Ace",
+)
+values = {
+    "Two": 2,
+    "Three": 3,
+    "Four": 4,
+    "Five": 5,
+    "Six": 6,
+    "Seven": 7,
+    "Eight": 8,
+    "Nine": 9,
+    "Ten": 10,
+    "Jack": 10,
+    "Queen": 10,
+    "King": 10,
+    "Ace": 11,
+}
 playing = True
 # Classes
 class Card:
     def __init__(self, suit, rank):
         self.suit = suit
         self.rank = rank
+
     def __str__(self):
         return self.rank + " of " + self.suit
+
+
 class Deck:
     def __init__(self):
         self.deck = []
         for suit in suits:
             for rank in ranks:
-                self.deck.append(Card(suit,rank))
+                self.deck.append(Card(suit, rank))
+
     def __str__(self):
-        deck_composition = ' '
+        deck_composition = " "
         for card in self.deck:
-            deck_composition += '\n'+ card.__str__()
+            deck_composition += "\n" + card.__str__()
         return "The deck has: " + deck_composition
+
     def shuffle(self):
         random.shuffle(self.deck)
+
     def deal(self):
         single_card = self.deck.pop()
         return single_card
+
+
 # test_deck = Deck()
 # print(test_deck)
 # test_deck.shuffle()
@@ -35,60 +72,79 @@ class Hand:
     def __init__(self):
         self.cards = []
         self.value = 0
-        self.aces = 0 # Aces can be 1 or 11 in Blackjack
-    def add_card(self,card):
+        self.aces = 0  # Aces can be 1 or 11 in Blackjack
+
+    def add_card(self, card):
         self.cards.append(card)
         self.value += values[card.rank]
-        if card.rank == 'Ace':
+        if card.rank == "Ace":
             self.aces += 1
+
     def adjust_for_ace(self):
         while self.value > 21 and self.aces:
             self.value -= 10
             self.aces -= 1
+
+
 # test_deck = Deck()
 # test_deck.shuffle()
 # test_player = Hand()
 # pulled_card = test_deck.deal()
 # print(pulled_card)
 # test_player.add_card(pulled_card)
-# print(test_player.value)    
+# print(test_player.value)
 class Chips:
-    def __init__(self, total = 100):
+    def __init__(self, total=100):
         self.total = total
         self.bet = 0
+
     def win_bet(self):
         self.total += self.bet
+
     def lose_bet(self):
         self.total -= self.bet
+
 
 def take_bet(chips):
     while True:
         try:
-            chips.bet = int(input("You have 100 chips. How many chips would you like to bet? "))
+            chips.bet = int(
+                input("You have 100 chips. How many chips would you like to bet? ")
+            )
         except:
             print("Sorry, please provide an integer")
         else:
             if chips.bet > chips.total:
-                print("Sorry, you do not have enough chips! You have {}".format(chips.total))
+                print(
+                    "Sorry, you do not have enough chips! You have {}".format(
+                        chips.total
+                    )
+                )
             else:
                 break
+
+
 def hit(deck, hand):
     single_card = deck.deal()
     hand.add_card(single_card)
     hand.adjust_for_ace()
+
+
 def hit_or_stand(deck, hand):
     global playing
     while True:
         x = input("Hit or Stand? 'h' or 's' ")
-        if x[0].lower() == 'h':
-            hit(deck,hand)
-        elif x[0].lower() == 's':
+        if x[0].lower() == "h":
+            hit(deck, hand)
+        elif x[0].lower() == "s":
             print("Player Stands, Dealer's Turn")
             playing = False
         else:
             print("Sorry, please try again")
             continue
         break
+
+
 def show_some(player, dealer):
     print("\nDealer's Hand: ")
     print("(First card hidden)")
@@ -96,6 +152,8 @@ def show_some(player, dealer):
     print("\nPlayer's Hand:")
     for card in player.cards:
         print(card)
+
+
 def show_all(player, dealer):
     print("\nDealer's Hand: ")
     for card in dealer.cards:
@@ -105,21 +163,32 @@ def show_all(player, dealer):
     for card in player.cards:
         print(card)
     print(f"Value of Player's Hand is: {player.value}")
+
+
 # End game scenarios
 def player_busts(player, dealer, chips):
     print("     Player busts!")
     chips.lose_bet()
+
+
 def player_wins(player, dealer, chips):
     print("     Player wins!")
     chips.win_bet()
+
+
 def dealer_busts(player, dealer, chips):
     print("     Player wins! Dealer busts!")
     chips.win_bet()
+
+
 def dealer_wins(player, dealer, chips):
     print("     Dealer wins!")
     chips.lose_bet()
+
+
 def push(player, dealer):
     print("     Dealer and player tie, push!")
+
 
 while True:
     print("Welcome to Blackjack")
@@ -152,11 +221,10 @@ while True:
             player_wins(player_hand, dealer_hand, player_chips)
         else:
             push(player_hand, dealer_hand)
-    print('\nPlayer total chips are at: {}'.format(player_chips.total))
+    print("\nPlayer total chips are at: {}".format(player_chips.total))
     new_game = input("Would you like to play another hand? 'y' or 'n' ")
-    if new_game[0].lower() == 'y':
+    if new_game[0].lower() == "y":
         playing = True
         continue
     else:
         print("Thanks for playing")
-
